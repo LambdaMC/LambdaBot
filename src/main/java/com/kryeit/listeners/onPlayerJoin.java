5e5c61b77ea36d8e47abb85459b6c61e2f01f211
muriplz <46825658+muriplz@users.noreply.github.com>
2023-06-20 20:08:10 +0200

    Añadido que se muestren los jugadores online

--- src/main/java/com/kryeit/listeners/onPlayerJoin.java
+++ src/main/java/com/kryeit/listeners/onPlayerJoin.java
@@ -1,5 +1,7 @@
 package com.kryeit.listeners;
 
+import net.dv8tion.jda.api.entities.Activity;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,5 +12,7 @@ public class onPlayerJoin implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
+        int playerCount = Bukkit.getOnlinePlayers().size();
+        api.getPresence().setActivity(Activity.watching("a " + playerCount + " jugadores"));
     }
 }
--- src/main/java/com/kryeit/listeners/onPlayerLeave.java
+++ src/main/java/com/kryeit/listeners/onPlayerLeave.java
@@ -1,8 +1,21 @@
 package com.kryeit.listeners;
 
+import net.dv8tion.jda.api.entities.Activity;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.net.http.WebSocket;
 
+import static com.kryeit.LambdaBot.api;
+
 public class onPlayerLeave implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerQuitEvent e) {
+        int playerCount = Bukkit.getOnlinePlayers().size();
+        api.getPresence().setActivity(Activity.watching("a " + playerCount + " jugadores"));
+    }
 }
