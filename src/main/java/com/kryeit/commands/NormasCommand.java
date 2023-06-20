b5bc25a08541ea1e5bb1209a6d259b95c705fa5b
males75 <robots20081@gmail.com>
2023-06-20 17:16:33 +0200

    Normas
    ...

--- src/main/java/com/kryeit/commands/NormasCommand.java
+++ src/main/java/com/kryeit/commands/NormasCommand.java
@@ -20,14 +20,15 @@ public class NormasCommand extends ListenerAdapter {
             EmbedBuilder eb = new EmbedBuilder();
             
             // Set the properties of your embed
-            eb.setColor(Color.ORANGE);
+            eb.setColor(Color.GREEN);
             eb.setThumbnail("https://kryeit.com/images/lambdacraft.png");// replace imageUrlString with your actual URL
+            eb.setImage("https://images-ext-2.discordapp.net/external/0lOfZf6LqZepiqgAWCHM8JkV9Ef7csRXOXauMeRHaaQ/https/kryeit.com/images/lambda_title.png?width=1440&height=219");
             eb.setFooter("LambdaCraft");
             eb.setAuthor("Normas de LambdaCraft");
             eb.addField("1. \uD83E\uDD16 Respeta el servidor", "- No construyas máquinas de lag\n- Utiliza los canales de Discord según su función", false);
             eb.addField("2. \u26CF Respeta los jugadores", "- No hagas protecciones cerca de otros jugadores, a no ser que sea cerca de postes\n- Trata a los demás como te gustaría que te tratasen", false);
             eb.addField("3. \uD83C\uDFE0 Respeta zonas comunes", "- No destruyas zonas comunes\n- Respeta las granjas públicas", false);
-            eb.addField("Contacta a los administradores si encuentras algún error", muriplz + " y " + ender, false);
+            eb.addField("Contacta a los administradores si encuentras algún error", muriplz.getAsMention() + " y " + ender.getAsMention(), false);
 
             // Send the embed
             event.replyEmbeds(eb.build()).queue();
