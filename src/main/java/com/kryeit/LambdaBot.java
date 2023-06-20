97bb50931a7eccfe5c417f63e7ef1234fdb983ff
muriplz <46825658+muriplz@users.noreply.github.com>
2023-06-20 17:10:02 +0200

    cositas

--- src/main/java/com/kryeit/LambdaBot.java
+++ src/main/java/com/kryeit/LambdaBot.java
@@ -7,16 +7,20 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.entities.UserSnowflake;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
+import java.nio.channels.Channel;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class LambdaBot {
 
     public static Guild guild;
-    public static String muriplz = User.fromId("235400519365951488").getAsMention();
-    public static String ender = User.fromId("774275869300228096").getAsMention();
+    public static UserSnowflake muriplz = User.fromId("235400519365951488");
+    public static UserSnowflake ender = User.fromId("774275869300228096");
+    public static MessageChannel normas = guild.getTextChannelById("1119277621696549086");
 
     public static void main(String[] arguments) throws Exception
     {
--- src/main/java/com/kryeit/commands/InfoCommand.java
+++ src/main/java/com/kryeit/commands/InfoCommand.java
@@ -9,8 +9,7 @@ import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 import java.awt.*;
 import java.net.URL;
 
-import static com.kryeit.LambdaBot.ender;
-import static com.kryeit.LambdaBot.muriplz;
+import static com.kryeit.LambdaBot.*;
 
 public class InfoCommand extends ListenerAdapter {
     @Override
@@ -24,9 +23,9 @@ public class InfoCommand extends ListenerAdapter {
             eb.setThumbnail("https://kryeit.com/images/lambdacraft.png");// replace imageUrlString with your actual URL
             eb.setFooter("LambdaCraft");
             eb.setAuthor("Información");
-            eb.addField("1. Normas", "- Leerlas para no sancionarte\n- Las puedes leer en #normas", false);
+            eb.addField("1. Normas", "- Leerlas para no sancionarte\n- Las puedes leer en " + normas.getAsMention(), false);
             eb.addField("2. IP del servidor", "- Utiliza el comando /ip\n- No utilizar muchas veces seguidas el comando", false);
-            eb.addField("Contacta a los administradores si encuentras algún error", muriplz + " y " + ender, false);
+            eb.addField("Contacta a los administradores si encuentras algún error", muriplz.getAsMention() + " y " + ender.getAsMention(), false);
 
             // Send the embed
             event.replyEmbeds(eb.build()).queue();
